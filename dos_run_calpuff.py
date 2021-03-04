795616d4cb666f49947f54a4697344d175971fe7
Dr Brian <14358664+bfree67@users.noreply.github.com>
2021-03-03 22:38:15 -0500

    Add files via upload

--- dos_run_calpuff.py
+++ dos_run_calpuff.py
@@ -11,26 +11,36 @@ Changes CONC.DAT output file to new name to prevent overwrite
 import os
 import shutil
 
-# location of CALPUFF exe file
+# location of CALPUFF .exe file
 calpuff_path = r"C:\Program Files (x86)\Lakes\CALPUFF View\Models_7"
 
 # CALPUFF model exe file
 calpuff = "calpuff_v7.2.1"
 
 # location of CALPUFF.INP files
-calpuff_inp_path = r"C:\Users\Brian\Documents\_Active\Hexagon\pre-incident\wrf\pre-incident"
+# IMPORTANT!!!! - Update path to WRF data in Subgroup 0a 
+calpuff_inp_path = r"C:\Users\Brian\Documents\_Active\Hexagon\pre-incident\wrf\pre-external"
 
-# location of CONC.DAT output files
+# location of CALPUFF CONC.DAT output files (need to run CALPOST for data)
 post_path = r"C:\Users\Brian\AppData\Local\VirtualStore\Program Files (x86)\Lakes\CALPUFF View\Models_7"
 
 # list of CALPUFF.INP file names
-calpuff_scenarios = ["CALPUFF", "CALPUFF_JUNE"]
+calpuff_scenarios = ['SOURCE_3',
+'SOURCE_4',
+'SOURCE_5',
+'SOURCE_6',
+'SOURCE_7',
+'SOURCE_8',
+'SOURCE_9',
+'SOURCE_10',
+'SOURCE_11',
+'SOURCE_12']
 
 for file_inp in calpuff_scenarios:
     
     cal_name = file_inp + '.INP'
     
-    print('Running CALPUFF model for ' + file_inp + '\n')
+    print('\nRunning CALPUFF model for ' + file_inp + '\n')
     os.chdir(calpuff_path)
     os.system(calpuff + ' ' + calpuff_inp_path + '\\' + cal_name)
     
