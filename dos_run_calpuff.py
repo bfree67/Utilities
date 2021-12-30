7ffb4ec30875c964ede4bfd4ed8c91447ec5798b
Dr Brian <brianfree67@hotmail.com>
2021-12-29 21:24:49 -0500

    Add files via upload

--- dos_run_calpuff.py
+++ dos_run_calpuff.py
@@ -1,52 +1,56 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Jan 30 16:15:53 2021
-
 Loads CALPUFF.INP files and runs CALPUFF 7
 Changes CONC.DAT output file to new name to prevent overwrite
-
 @author: Brian
 """
 
 import os
 import shutil
 
+home_path = os.getcwd()
+
 # location of CALPUFF .exe file
 calpuff_path = r"C:\Program Files (x86)\Lakes\CALPUFF View\Models_7"
 
 # CALPUFF model exe file
 calpuff = "calpuff_v7.2.1"
+calpost = "calpost_v7.1.0"
 
 # location of CALPUFF.INP files
 # IMPORTANT!!!! - Update path to WRF data in Subgroup 0a 
-calpuff_inp_path = r"C:\Users\Brian\Documents\_Active\Hexagon\pre-incident\wrf\pre-external"
+calpuff_inp_path = r"C:\TARS\AAAActive\Envirozone\sharjah\sharjah_test"
 
-# location of CALPUFF CONC.DAT output files (need to run CALPOST for data)
-post_path = r"C:\Users\Brian\AppData\Local\VirtualStore\Program Files (x86)\Lakes\CALPUFF View\Models_7"
+# IMPORTANT!!!! - Update calpost.inp file path in the pollutant folders
+calpost_inp_path = r"C:\TARS\AAAActive\Envirozone\sharjah\sharjah_test\sharjah_test_post\SO2"
 
-# list of CALPUFF.INP file names
-calpuff_scenarios = ['SOURCE_3',
-'SOURCE_4',
-'SOURCE_5',
-'SOURCE_6',
-'SOURCE_7',
-'SOURCE_8',
-'SOURCE_9',
-'SOURCE_10',
-'SOURCE_11',
-'SOURCE_12']
+#%%
 
+calpuff_scenarios = ['source1'] ## list of CALPUFF.INP file names
+
+calpost_scenarios = ['SO2'] ## make list of pollutants
+
+#%%
+## run model
 for file_inp in calpuff_scenarios:
     
     cal_name = file_inp + '.INP'
     
     print('\nRunning CALPUFF model for ' + file_inp + '\n')
+    
+    # Run CALPUFF model
     os.chdir(calpuff_path)
     os.system(calpuff + ' ' + calpuff_inp_path + '\\' + cal_name)
     
-    #rename CONC.DAT file
-    os.chdir(post_path)
-    shutil.copy('CONC.DAT', 'CONC_' + file_inp + '.DAT')
+    # run CALPOST for each pollutant
+    for post_file in calpost_scenarios:
+        
+        post_inp = post_file + '_C.INP'
+        
+        print('\nRunning CALPOST for ' + post_file + '\n')
+        
+        os.chdir(calpuff_path)
+        os.system(calpost + ' ' + calpost_inp_path + '\\' + post_inp)
 
-
-
+#set back to home directory
+os.chdir(home_path)
